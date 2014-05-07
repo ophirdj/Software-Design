c5794b5e0baa0f4ccb5b999b4f74c7f867146003
Ophir De Jager <ophirdj@gmail.com>
2014-05-07 20:20:22 +0300

    Tear down after class

--- twitter-knowledge-center/src/test/java/ac/il/technion/twc/SampleTest.java
+++ twitter-knowledge-center/src/test/java/ac/il/technion/twc/SampleTest.java
@@ -3,6 +3,7 @@ package ac.il.technion.twc;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,38 +11,42 @@ import org.junit.Test;
  * Small test given by the course staff
  */
 public class SampleTest {
-  /**
+	/**
    * 
    */
-  TwitterKnowledgeCenter $ = new TwitterKnowledgeCenter();
+	TwitterKnowledgeCenter $ = new TwitterKnowledgeCenter();
 
-  /**
-   * @throws Exception
-   */
-  @Before
-  public void setup() throws Exception {
-    $.cleanPersistentData();
-  }
+	/**
+	 * @throws Exception
+	 */
+	@Before
+	public void setup() throws Exception {
+		$.cleanPersistentData();
+	}
 
-  /**
-   * @throws Exception
-   */
-  @Test
-  public void sampleTest() throws Exception {
-    String[] lines =
-        new String[] { "04/04/2014 12:00:00, iddqd",
-            "05/04/2014 12:00:00, idkfa, iddqd" };
-    $.importData(lines);
-    lines = new String[] { "06/04/2014 13:00:00, 593393706" };
-    $.importData(lines);
-    $.setupIndex();
-    assertEquals("86400000", $.getLifetimeOfTweets("iddqd"));
-    assertArrayEquals(new String[] { "1,0", "0,0", "0,0", "0,0", "0,0", "1,0",
-        "1,1" }, $.getDailyHistogram());
-    lines = new String[] { "06/04/2014 11:00:00, 40624256" };
-    $.importData(lines);
-    $.setupIndex();
-    assertArrayEquals(new String[] { "2,0", "0,0", "0,0", "0,0", "0,0", "1,0",
-        "1,1" }, $.getDailyHistogram());
-  }
+	@After
+	public void tearDown() throws Exception {
+		$.cleanPersistentData();
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void sampleTest() throws Exception {
+		String[] lines = new String[] { "04/04/2014 12:00:00, iddqd",
+				"05/04/2014 12:00:00, idkfa, iddqd" };
+		$.importData(lines);
+		lines = new String[] { "06/04/2014 13:00:00, 593393706" };
+		$.importData(lines);
+		$.setupIndex();
+		assertEquals("86400000", $.getLifetimeOfTweets("iddqd"));
+		assertArrayEquals(new String[] { "1,0", "0,0", "0,0", "0,0", "0,0",
+				"1,0", "1,1" }, $.getDailyHistogram());
+		lines = new String[] { "06/04/2014 11:00:00, 40624256" };
+		$.importData(lines);
+		$.setupIndex();
+		assertArrayEquals(new String[] { "2,0", "0,0", "0,0", "0,0", "0,0",
+				"1,0", "1,1" }, $.getDailyHistogram());
+	}
 }
