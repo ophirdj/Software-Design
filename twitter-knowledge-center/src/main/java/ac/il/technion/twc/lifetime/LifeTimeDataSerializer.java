ecf00deeac9df2c2311e2e36c356a2bfc8e3d12b
Ophir De Jager <ophirdj@gmail.com>
2014-05-07 18:46:19 +0300

    Serializer to fix LifeTimeData bug: map serialization is fucked up

---
+++ twitter-knowledge-center/src/main/java/ac/il/technion/twc/lifetime/LifeTimeDataSerializer.java
@@ -0,0 +1,67 @@
+package ac.il.technion.twc.lifetime;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import ac.il.technion.twc.message.ID;
+import ac.il.technion.twc.message.tweet.BaseTweet;
+import ac.il.technion.twc.message.tweet.Retweet;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.reflect.TypeToken;
+
+public class LifeTimeDataSerializer implements JsonSerializer<LifeTimeData>,
+		JsonDeserializer<LifeTimeData> {
+
+	@Override
+	public LifeTimeData deserialize(final JsonElement json, final Type type,
+			final JsonDeserializationContext context) throws JsonParseException {
+		// System.out.println("deserialize called");
+		final JsonObject jsonObject = json.getAsJsonObject();
+		final Set<BaseTweet> baseTweets = context.deserialize(
+				jsonObject.get("baseTweets"), new TypeToken<Set<BaseTweet>>() {
+				}.getType());
+		final Set<Retweet> retweets = context.deserialize(
+				jsonObject.get("retweets"), new TypeToken<Set<Retweet>>() {
+				}.getType());
+		final Map<String, Long> m = context.deserialize(jsonObject.get("map"),
+				new TypeToken<Map<String, Long>>() {
+				}.getType());
+		final Map<ID, Long> map = new HashMap<>();
+		for (final Entry<String, Long> e : m.entrySet())
+			map.put(new ID(e.getKey()), e.getValue());
+		// for (final Entry<ID, Long> e : map.entrySet())
+		// System.out.println(e.getKey().id + ":" + e.getValue());
+		// for (final BaseTweet t : baseTweets)
+		// System.out.println(t.id().id + ", " + t.date());
+		// for (final Retweet t : retweets)
+		// System.out.println(t.id().id + ", " + t.date() + ", " +
+		// t.originId.id);
+		return new LifeTimeData(map, baseTweets, retweets);
+	}
+
+	@Override
+	public JsonElement serialize(final LifeTimeData lifeTime, final Type type,
+			final JsonSerializationContext context) {
+		// System.out.println("serialize called");
+		final JsonObject $ = new JsonObject();
+		$.add("baseTweets", context.serialize(lifeTime.baseTweets));
+		$.add("retweets", context.serialize(lifeTime.retweets));
+		final Map<String, Long> m = new HashMap<>();
+		for (final Entry<ID, Long> e : lifeTime.map.entrySet())
+			m.put(e.getKey().id, e.getValue());
+		$.add("map", context.serialize(m));
+		// System.out.println($);
+		return $;
+	}
+
+}
--- twitter-knowledge-center/src/test/java/ac/il/technion/twc/storage/StorageHandlerTest.java
+++ twitter-knowledge-center/src/test/java/ac/il/technion/twc/storage/StorageHandlerTest.java
@@ -21,87 +21,91 @@ import ac.il.technion.twc.God;
 import ac.il.technion.twc.histogram.DayHistogram;
 import ac.il.technion.twc.histogram.DayOfWeek;
 import ac.il.technion.twc.lifetime.LifeTimeData;
+import ac.il.technion.twc.lifetime.LifeTimeDataSerializer;
 import ac.il.technion.twc.message.ID;
 import ac.il.technion.twc.message.tweet.BaseTweet;
 import ac.il.technion.twc.message.tweet.Retweet;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
 
 public class StorageHandlerTest {
 
-  private final Gson gson;
-  private final FileHandler fileHandlingMock;
-  private final Path testPath = Paths.get("test/cache");
-  private final StorageHandler<DayHistogram> underTestHistogram;
-  private final StorageHandler<LifeTimeData> underTestLifeTime;
+	private final Gson gson;
+	private final FileHandler fileHandlingMock;
+	private final Path testPath = Paths.get("test/cache");
+	private final StorageHandler<DayHistogram> underTestHistogram;
+	private final StorageHandler<LifeTimeData> underTestLifeTime;
 
-  public StorageHandlerTest() {
-    fileHandlingMock = mock(FileHandler.class);
-    gson = new Gson();
-    underTestHistogram = new StorageHandler<>(gson, testPath, fileHandlingMock);
-    underTestLifeTime = new StorageHandler<>(gson, testPath, fileHandlingMock);
-  }
+	public StorageHandlerTest() {
+		fileHandlingMock = mock(FileHandler.class);
+		gson = new GsonBuilder().registerTypeAdapter(LifeTimeData.class,
+				new LifeTimeDataSerializer()).create();
+		underTestHistogram = new StorageHandler<>(gson, testPath,
+				fileHandlingMock);
+		underTestLifeTime = new StorageHandler<>(gson, testPath,
+				fileHandlingMock);
+	}
 
-  @Test
-  public void histogramLoadWithoutPreviousStoreShouldReturnDefaultValue()
-      throws IOException {
-    when(fileHandlingMock.load(testPath)).thenThrow(new IOException());
-    final DayHistogram mockHistogram = mock(DayHistogram.class);
-    assertEquals(mockHistogram, underTestHistogram.load(mockHistogram));
-    verify(fileHandlingMock).load(testPath);
-  }
+	@Test
+	public void histogramLoadWithoutPreviousStoreShouldReturnDefaultValue()
+			throws IOException {
+		when(fileHandlingMock.load(testPath)).thenThrow(new IOException());
+		final DayHistogram mockHistogram = mock(DayHistogram.class);
+		assertEquals(mockHistogram, underTestHistogram.load(mockHistogram));
+		verify(fileHandlingMock).load(testPath);
+	}
 
-  @Test
-  public void histogramLoadWithPreviousStoreShouldReturnPreviousStore()
-      throws IOException {
-    final DayHistogram mockHistogramDefault =
-        God.injector.getInstance(Key.get(DayHistogram.class,
-            Names.named("default")));
-    final Map<DayOfWeek, Integer> tweets = new EnumMap<>(DayOfWeek.class);
-    final Map<DayOfWeek, Integer> retweets = new EnumMap<>(DayOfWeek.class);
-    for (final DayOfWeek day : DayOfWeek.values()) {
-      tweets.put(day, 0);
-      retweets.put(day, 0);
-    }
-    tweets.put(DayOfWeek.WEDNESDAY, 1);
-    final DayHistogram returnVal = new DayHistogram(tweets, retweets);
-    when(fileHandlingMock.load(testPath)).thenReturn(
-        gson.toJson(returnVal, DayHistogram.class));
-    assertEquals(returnVal, underTestHistogram.load(mockHistogramDefault));
-  }
+	@Test
+	public void histogramLoadWithPreviousStoreShouldReturnPreviousStore()
+			throws IOException {
+		final DayHistogram mockHistogramDefault = God.injector.getInstance(Key
+				.get(DayHistogram.class, Names.named("default")));
+		final Map<DayOfWeek, Integer> tweets = new EnumMap<>(DayOfWeek.class);
+		final Map<DayOfWeek, Integer> retweets = new EnumMap<>(DayOfWeek.class);
+		for (final DayOfWeek day : DayOfWeek.values()) {
+			tweets.put(day, 0);
+			retweets.put(day, 0);
+		}
+		tweets.put(DayOfWeek.WEDNESDAY, 1);
+		final DayHistogram returnVal = new DayHistogram(tweets, retweets);
+		when(fileHandlingMock.load(testPath)).thenReturn(
+				gson.toJson(returnVal, DayHistogram.class));
+		assertEquals(returnVal, underTestHistogram.load(mockHistogramDefault));
+	}
 
-  @Test
-  public void lifeTimeLoadWithoutPreviousStoreShouldReturnDefaultValue()
-      throws IOException {
-    when(fileHandlingMock.load(testPath)).thenThrow(new IOException());
-    final LifeTimeData mockLifeTime = mock(LifeTimeData.class);
-    assertEquals(mockLifeTime, underTestLifeTime.load(mockLifeTime));
-    verify(fileHandlingMock).load(testPath);
-  }
+	@Test
+	public void lifeTimeLoadWithoutPreviousStoreShouldReturnDefaultValue()
+			throws IOException {
+		when(fileHandlingMock.load(testPath)).thenThrow(new IOException());
+		final LifeTimeData mockLifeTime = mock(LifeTimeData.class);
+		assertEquals(mockLifeTime, underTestLifeTime.load(mockLifeTime));
+		verify(fileHandlingMock).load(testPath);
+	}
 
-  @Test
-  public void lifeTimeLoadWithPreviousStoreShouldReturnPreviousStore()
-      throws IOException {
-    final LifeTimeData mockLifeTime =
-        God.injector.getInstance(Key.get(LifeTimeData.class,
-            Names.named("default")));
-    final Set<BaseTweet> baseTweets = new HashSet<>();
-    final Set<Retweet> retweets = new HashSet<>();
-    final Map<ID, Long> map = new HashMap<>();
-    final BaseTweet baseTweet = new BaseTweet(new Date(1L), new ID("123"));
-    baseTweets.add(baseTweet);
-    final Retweet retweet =
-        new Retweet(new Date(101L), new ID("456"), new ID("123"));
-    retweets.add(retweet);
-    map.put(baseTweet.id(), 100L);
+	@Test
+	public void lifeTimeLoadWithPreviousStoreShouldReturnPreviousStore()
+			throws IOException {
+		final LifeTimeData mockLifeTime = God.injector.getInstance(Key.get(
+				LifeTimeData.class, Names.named("default")));
+		final Set<BaseTweet> baseTweets = new HashSet<>();
+		final Set<Retweet> retweets = new HashSet<>();
+		final Map<ID, Long> map = new HashMap<>();
+		final BaseTweet baseTweet = new BaseTweet(new Date(1L), new ID("123"));
+		baseTweets.add(baseTweet);
+		final Retweet retweet = new Retweet(new Date(101L), new ID("456"),
+				new ID("123"));
+		retweets.add(retweet);
+		map.put(baseTweet.id(), 100L);
 
-    final LifeTimeData returnVal = new LifeTimeData(map, baseTweets, retweets);
-    String json = gson.toJson(returnVal, LifeTimeData.class);
-    when(fileHandlingMock.load(testPath)).thenReturn(
-        json);
-    LifeTimeData load = underTestLifeTime.load(mockLifeTime);
-    assertEquals(returnVal, load);
-  }
+		final LifeTimeData returnVal = new LifeTimeData(map, baseTweets,
+				retweets);
+		final String json = gson.toJson(returnVal, LifeTimeData.class);
+		when(fileHandlingMock.load(testPath)).thenReturn(json);
+		final LifeTimeData load = underTestLifeTime.load(mockLifeTime);
+
+		assertEquals(returnVal, load);
+	}
 }
