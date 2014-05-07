fe076ce9d65782eeebcc3c03ec9b46576a2f4bc7
Ziv Ronen <akarks@gmail.com>
2014-05-07 20:01:40 +0300

    Fix test

--- twitter-knowledge-center/src/main/java/ac/il/technion/twc/message/tweet/Tweet.java
+++ twitter-knowledge-center/src/main/java/ac/il/technion/twc/message/tweet/Tweet.java
@@ -12,61 +12,60 @@ import ac.il.technion.twc.message.Message;
  */
 public abstract class Tweet implements Message {
 
-	private final Date date;
-	private final ID id;
+  private final Long date;
+  private final ID id;
 
-	/**
-	 * @param date
-	 *            Time when tweet was published.
-	 * @param id
-	 *            Unique ID for this tweet.
-	 */
-	public Tweet(final Date date, final ID id) {
-		this.date = date;
-		this.id = id;
-	}
+  /**
+   * @param date
+   *          Time when tweet was published.
+   * @param id
+   *          Unique ID for this tweet.
+   */
+  public Tweet(final Date date, final ID id) {
+    this.date = date.getTime();
+    this.id = id;
+  }
 
-	@Override
-	public ID id() {
-		return id;
-	}
+  @Override
+  public ID id() {
+    return id;
+  }
 
-	/**
-	 * @return Time when tweet was published.
-	 */
-	public Date date() {
-		return date;
-	}
+  /**
+   * @return Time when tweet was published.
+   */
+  public Date date() {
+    return new Date(date);
+  }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (date == null ? 0 : date.hashCode());
-		result = prime * result + (id == null ? 0 : id.hashCode());
-		return result;
-	}
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (id == null ? 0 : id.hashCode());
+    return result;
+  }
 
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final Tweet other = (Tweet) obj;
-		if (date == null) {
-			if (other.date != null)
-				return false;
-		} else if (!date.equals(other.date))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    final Tweet other = (Tweet) obj;
+    if (date == null) {
+      if (other.date != null)
+        return false;
+    } else if (!date.equals(other.date))
+      return false;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
 
 }
