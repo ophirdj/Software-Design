8e93cefee712234e3df5f85c7d8b900d0bf119b6
Ophir De Jager <ophirdj@gmail.com>
2014-05-07 20:18:54 +0300

    Junit params tests

--- twitter-knowledge-center/src/test/java/ac/il/technion/twc/message/tweet/builder/TweetBuilderTest.java
+++ twitter-knowledge-center/src/test/java/ac/il/technion/twc/message/tweet/builder/TweetBuilderTest.java
@@ -1,13 +1,18 @@
 package ac.il.technion.twc.message.tweet.builder;
 
+import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertEquals;
 
 import java.text.ParseException;
 import java.util.GregorianCalendar;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 
 import ac.il.technion.twc.God;
 import ac.il.technion.twc.message.ID;
@@ -15,53 +20,76 @@ import ac.il.technion.twc.message.tweet.BaseTweet;
 import ac.il.technion.twc.message.tweet.Retweet;
 import ac.il.technion.twc.message.tweet.Tweet;
 
+/**
+ * Tests for {@link TweetBuilder}.
+ * 
+ * @author Ophir De Jager
+ * 
+ */
+
+@RunWith(JUnitParamsRunner.class)
 public class TweetBuilderTest {
-	
-	public final @Rule ExpectedException thrown = ExpectedException.none();
 
-	
-	private final TweetBuilder underTest = God.injector.getInstance(TweetBuilder.class);
-	
-	@Test
-	public final void parseBaseTweetSouldReturnBaseTweet() throws ParseException {
-		Tweet tweet = underTest.parse("15/05/2013 13:08:07, 334611141902872576");
-		assertEquals(new BaseTweet(new GregorianCalendar(2013, 4, 15, 13, 8, 7).getTime(), new ID("334611141902872576")), tweet);
+	/**
+	 * Rulde for verifying correct error messages of thrown exceptions.
+	 */
+	public final @Rule
+	ExpectedException thrown = ExpectedException.none();
+
+	private final TweetBuilder underTest = God.injector
+			.getInstance(TweetBuilder.class);
+
+	@SuppressWarnings("unused")
+	// used by JunitParams
+	private Object[] correctTweets() {
+		return $(
+				$(new BaseTweet(new GregorianCalendar(2013, 4, 15, 13, 8, 7)
+						.getTime(), new ID("334611141902872576")),
+						"15/05/2013 13:08:07, 334611141902872576"),
+				$(new Retweet(new GregorianCalendar(2013, 4, 15, 5, 23, 7)
+						.getTime(), new ID("334611141890285568"), new ID(
+						"334611004342280192")),
+						"15/05/2013 05:23:07, 334611141890285568, 334611004342280192"),
+				$(new BaseTweet(new GregorianCalendar(2013, 4, 15, 0, 0, 0)
+						.getTime(), new ID("334611141902872576")),
+						"15/05/2013 00:00:00, 334611141902872576"));
 	}
 
+	/**
+	 * Test method for {@link TweetBuilder#parse(String)}.
+	 * 
+	 * @param expected
+	 * @param tweetStr
+	 * @throws ParseException
+	 */
+	@Parameters(method = "correctTweets")
 	@Test
-	public final void parseRetweetSouldReturnRetweet() throws ParseException {
-		Tweet tweet = underTest.parse("15/05/2013 05:23:07, 334611141890285568, 334611004342280192");
-		assertEquals(new Retweet(new GregorianCalendar(2013, 4, 15, 5, 23, 7).getTime(), new ID("334611141890285568"), new ID("334611004342280192")), tweet);
+	public void parseCorrect(final Tweet expected, final String tweetStr)
+			throws ParseException {
+		assertEquals(expected, underTest.parse(tweetStr));
 	}
-	
+
+	@SuppressWarnings("unused")
+	// used by JunitParams
+	private Object[] badTweets() {
+		return $(
+				$("15/05/2013 13:08:07"),
+				$("15/05/2013 13:08:07, 334611141902872576, 334611141902872577, 334611141902872578"),
+				$("334611141902872576, 334611141902872577"));
+	}
+
+	/**
+	 * Test method for {@link TweetBuilder#parse(String)}.
+	 * 
+	 * @param tweetStr
+	 * @throws ParseException
+	 */
+	@Parameters(method = "badTweets")
 	@Test
-	public final void parseWithNoIdShouldThrowParseException() throws ParseException {
-		String tweet = "15/05/2013 13:08:07";
+	public void parseBad(final String tweetStr) throws ParseException {
 		thrown.expect(ParseException.class);
-		thrown.expectMessage("Bad tweet format: " + tweet);
-		underTest.parse(tweet);
-	}
-	
-	@Test
-	public final void parseWith3IdsShouldThrowParseException() throws ParseException {
-		String tweet = "15/05/2013 13:08:07, 334611141902872576, 334611141902872577, 334611141902872578";
-		thrown.expect(ParseException.class);
-		thrown.expectMessage("Bad tweet format: " + tweet);
-		underTest.parse(tweet);
-	}
-	
-	@Test
-	public final void parseWithNoDateShouldThrowParseException() throws ParseException {
-		String tweet = "334611141902872576, 334611141902872577";
-		thrown.expect(ParseException.class);
-		thrown.expectMessage("Bad tweet format: " + tweet);
-		underTest.parse(tweet);
-	}
-	
-	@Test
-	public final void parseTweetAtMidnight00SouldReturnTweet() throws ParseException {
-		Tweet tweet = underTest.parse("15/05/2013 00:00:00, 334611141902872576");
-		assertEquals(new BaseTweet(new GregorianCalendar(2013, 4, 15, 0, 0, 0).getTime(), new ID("334611141902872576")), tweet);
+		thrown.expectMessage("Bad tweet format: " + tweetStr);
+		underTest.parse(tweetStr);
 	}
 
 }
