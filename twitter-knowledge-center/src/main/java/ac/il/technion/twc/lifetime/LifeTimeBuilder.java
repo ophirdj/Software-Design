36dd45dae6b74376fa979fa77ee5e9e62a5c40a7
Ziv Ronen <akarks@gmail.com>
2014-05-07 18:11:01 +0300

    Fix flash action

--- twitter-knowledge-center/src/main/java/ac/il/technion/twc/lifetime/LifeTimeBuilder.java
+++ twitter-knowledge-center/src/main/java/ac/il/technion/twc/lifetime/LifeTimeBuilder.java
@@ -7,7 +7,6 @@ import ac.il.technion.twc.message.visitor.MessagePropertyBuilder;
 import ac.il.technion.twc.storage.StorageHandler;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 
 /**
@@ -17,56 +16,55 @@ import com.google.inject.name.Named;
  * @author Ophir De Jager
  * 
  */
-@Singleton
 public class LifeTimeBuilder extends MessagePropertyBuilder<LifeTimeData> {
 
-	private final LifeTimeData tweets;
-	private final TransitiveRootFinder baseTweetFinder;
+  private final LifeTimeData tweets;
+  private final TransitiveRootFinder baseTweetFinder;
 
-	/**
-	 * @param storageHandler
-	 *            A storage handler for {@link LifeTimeData}.
-	 * @param rootFinder
-	 *            Finds base tweet for each tweet.
-	 * @param defaultLifeTime
-	 *            Default state of tweets if nothing was loadeds.
-	 */
-	@Inject
-	public LifeTimeBuilder(final StorageHandler<LifeTimeData> storageHandler,
-			final TransitiveRootFinder rootFinder,
-			@Named("default") final LifeTimeData defaultLifeTime) {
-		super(storageHandler);
-		tweets = storageHandler.load(defaultLifeTime);
-		baseTweetFinder = rootFinder;
-		baseTweetFinder.addBaseTweets(tweets.baseTweets);
-		baseTweetFinder.addRetweets(tweets.retweets);
-	}
+  /**
+   * @param storageHandler
+   *          A storage handler for {@link LifeTimeData}.
+   * @param rootFinder
+   *          Finds base tweet for each tweet.
+   * @param defaultLifeTime
+   *          Default state of tweets if nothing was loadeds.
+   */
+  @Inject
+  public LifeTimeBuilder(final StorageHandler<LifeTimeData> storageHandler,
+      final TransitiveRootFinder rootFinder,
+      @Named("default") final LifeTimeData defaultLifeTime) {
+    super(storageHandler);
+    tweets = storageHandler.load(defaultLifeTime);
+    baseTweetFinder = rootFinder;
+    baseTweetFinder.addBaseTweets(tweets.baseTweets);
+    baseTweetFinder.addRetweets(tweets.retweets);
+  }
 
-	@Override
-	public Void visit(final BaseTweet t) {
-		tweets.baseTweets.add(t);
-		baseTweetFinder.addTweet(t);
-		return null;
-	}
+  @Override
+  public Void visit(final BaseTweet t) {
+    tweets.baseTweets.add(t);
+    baseTweetFinder.addTweet(t);
+    return null;
+  }
 
-	@Override
-	public Void visit(final Retweet t) {
-		tweets.retweets.add(t);
-		baseTweetFinder.addTweet(t);
-		return null;
-	}
+  @Override
+  public Void visit(final Retweet t) {
+    tweets.retweets.add(t);
+    baseTweetFinder.addTweet(t);
+    return null;
+  }
 
-	@Override
-	protected LifeTimeData getResult() {
-		for (final Retweet t : tweets.retweets)
-			try {
-				final BaseTweet base = baseTweetFinder.findRoot(t);
-				tweets.map.put(base.id(), Math.max(t.date().getTime()
-						- base.date().getTime(), !tweets.map.containsKey(base
-						.id()) ? 0L : tweets.map.get(base.id())));
-			} catch (final NoRootFoundException e) {
-				continue;
-			}
-		return tweets;
-	}
+  @Override
+  protected LifeTimeData getResult() {
+    for (final Retweet t : tweets.retweets)
+      try {
+        final BaseTweet base = baseTweetFinder.findRoot(t);
+        tweets.map.put(base.id(), Math.max(t.date().getTime()
+            - base.date().getTime(), !tweets.map.containsKey(base.id()) ? 0L
+            : tweets.map.get(base.id())));
+      } catch (final NoRootFoundException e) {
+        continue;
+      }
+    return tweets;
+  }
 }
