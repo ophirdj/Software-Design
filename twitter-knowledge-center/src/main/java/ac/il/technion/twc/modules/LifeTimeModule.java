34fec832ef2ede32464ce8161ab0598c2f985185
Ziv Ronen <akarks@gmail.com>
2014-05-07 15:06:08 +0300

    Fix modules

--- twitter-knowledge-center/src/main/java/ac/il/technion/twc/modules/LifeTimeModule.java
+++ twitter-knowledge-center/src/main/java/ac/il/technion/twc/modules/LifeTimeModule.java
@@ -10,6 +10,7 @@ import ac.il.technion.twc.lifetime.LifeTimeData;
 import ac.il.technion.twc.message.ID;
 import ac.il.technion.twc.message.tweet.BaseTweet;
 import ac.il.technion.twc.message.tweet.Retweet;
+import ac.il.technion.twc.storage.FileHandler;
 import ac.il.technion.twc.storage.StorageHandler;
 
 import com.google.gson.Gson;
@@ -29,41 +30,42 @@ import com.google.inject.name.Named;
  */
 public class LifeTimeModule extends AbstractModule {
 
-	@Override
-	protected void configure() {
-		// nothing to do here...
-	}
+  @Override
+  protected void configure() {
+    // nothing to do here...
+  }
 
-	/**
-	 * @param storageDir
-	 *            Root storage directory for properties.
-	 * @return A handler for life time property storage
-	 */
-	@Provides
-	StorageHandler<LifeTimeData> lifeTimeStorage(
-			@Named("storage directory") final Path storageDir) {
-		return new StorageHandler<>(new Gson(), storageDir.resolve("life_time"));
-	}
+  /**
+   * @param storageDir
+   *          Root storage directory for properties.
+   * @return A handler for life time property storage
+   */
+  @Provides
+  StorageHandler<LifeTimeData> lifeTimeStorage(
+      @Named("storage directory") final Path storageDir) {
+    return new StorageHandler<>(new Gson(), storageDir.resolve("life_time"),
+        new FileHandler());
+  }
 
-	/**
-	 * @param lifeTimeBuilder
-	 * @param defaultLifeTimeMap
-	 * @return displayer for life time property
-	 */
-	@Provides
-	LifeTimeCache lifeTimeCache(final LifeTimeBuilder lifeTimeBuilder,
-			@Named("default") final LifeTimeData defaultLifeTimeMap) {
-		return new LifeTimeCache(lifeTimeBuilder.loadResult(defaultLifeTimeMap));
-	}
+  /**
+   * @param lifeTimeBuilder
+   * @param defaultLifeTimeMap
+   * @return displayer for life time property
+   */
+  @Provides
+  LifeTimeCache lifeTimeCache(final LifeTimeBuilder lifeTimeBuilder,
+      @Named("default") final LifeTimeData defaultLifeTimeMap) {
+    return new LifeTimeCache(lifeTimeBuilder.loadResult(defaultLifeTimeMap));
+  }
 
-	/**
-	 * @return an empty data for life time property (first usage)
-	 */
-	@Provides
-	@Named("default")
-	LifeTimeData defaultLifeTimeMap() {
-		return new LifeTimeData(new HashMap<ID, Long>(),
-				new HashSet<BaseTweet>(), new HashSet<Retweet>());
-	}
+  /**
+   * @return an empty data for life time property (first usage)
+   */
+  @Provides
+  @Named("default")
+  LifeTimeData defaultLifeTimeMap() {
+    return new LifeTimeData(new HashMap<ID, Long>(), new HashSet<BaseTweet>(),
+        new HashSet<Retweet>());
+  }
 
 }
