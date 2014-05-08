0f41d40a2cc9ef8807f29ed981c01c6a9d7d98bf
Ophir De Jager <ophirdj@gmail.com>
2014-05-08 17:43:30 +0300

    Time constraints tests (currently fail)

--- twitter-knowledge-center/src/test/java/ac/il/technion/twc/TimeConstraintsTest.java
+++ twitter-knowledge-center/src/test/java/ac/il/technion/twc/TimeConstraintsTest.java
@@ -1,14 +1,20 @@
 package ac.il.technion.twc;
 
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import static org.junit.Assert.assertTrue;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.Timeout;
+
+import com.google.inject.Key;
+import com.google.inject.name.Names;
 
 /**
  * Assert time constraints of methods of {@link TwitterKnowledgeCenter}.
@@ -18,26 +24,22 @@ import org.junit.rules.Timeout;
  */
 public class TimeConstraintsTest {
 
-	@Rule
-	public Timeout globalTimeout = new Timeout(2000); // FIXME change to ns
-														// (currently ms)
+	private static final int BASE_TWEETS = 322;
+	private static String[] lines = generateTweets(BASE_TWEETS, 5, 3, 0);
+	private static final int linesLengthApproximation = 10000;
 
-	private static String[] lines;
-	private static final int linesLength = 98; // must define as constant for
-												// JUnit annotations to use.
-
+	// didn't import data
+	private final TwitterKnowledgeCenter tkcImportData = new TwitterKnowledgeCenter();
+	// imported data but didn't set up index
+	private final TwitterKnowledgeCenter tkcSetupIndex = new TwitterKnowledgeCenter();
+	// imported data & set up index
 	private final TwitterKnowledgeCenter $ = new TwitterKnowledgeCenter();
 
-	/**
-	 * Load tweets.
-	 * 
-	 * @throws Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		lines = Files.readAllLines(
-				Paths.get("src/test/resources/small_sample.txt"),
-				Charset.defaultCharset()).toArray(new String[0]);
+	@Before
+	public void setUp() throws Exception {
+		tkcSetupIndex.importData(lines);
+		$.importData(lines);
+		$.setupIndex();
 	}
 
 	/**
@@ -47,53 +49,154 @@ public class TimeConstraintsTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		tkcSetupIndex.cleanPersistentData();
 		$.cleanPersistentData();
 	}
 
-	/**
-	 * Test method for {@link TwitterKnowledgeCenter#importData(String[])}
-	 * 
-	 * @throws Exception
-	 */
-	@Test(timeout = 2 * linesLength)
-	public final void importDataShouldRun2msForEachTweet() throws Exception {
+	@Test
+	public final void importData() throws Exception {
+		final long start = System.currentTimeMillis();
 		$.importData(lines);
+		final long end = System.currentTimeMillis();
+		assertTrue("Took " + (end - start) + " millis instead of " + 2
+				* lines.length, end - start <= 2 * lines.length);
 	}
 
-	/**
-	 * Test method for {@link TwitterKnowledgeCenter#setupIndex()}
-	 * 
-	 * @throws Exception
-	 */
-	public final void setupIndexSouldRun2000nsForEachTweet() throws Exception {
+	@Test
+	public final void setupIndex() throws Exception {
 		$.importData(lines);
-		// TODO: setup timeout
+		final long start = System.currentTimeMillis();
 		$.setupIndex();
+		final long end = System.currentTimeMillis();
+		assertTrue("Took " + (end - start) + " millis instead of " + 2
+				* lines.length / 1000, end - start <= 2 * lines.length / 1000);
 	}
 
-	/**
-	 * Test method for {@link TwitterKnowledgeCenter#getDailyHistogram()}
-	 * 
-	 * @throws Exception
-	 */
-	public final void getDailyHistogramSouldRun2000ns() throws Exception {
+	@Test
+	public final void getDailyHistogram() throws Exception {
 		$.importData(lines);
 		$.setupIndex();
-		// TODO: setup timeout
-		$.setupIndex();
+		final long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000; ++i)
+			$.getDailyHistogram();
+		final long end = System.currentTimeMillis();
+		assertTrue("Took " + (end - start) + " millis instead of 20", end
+				- start <= 20);
 	}
 
-	/**
-	 * Test method for
-	 * {@link TwitterKnowledgeCenter#getLifetimeOfTweets(String)}
-	 * 
-	 * @throws Exception
-	 */
-	public final void getLifeTimeOfTweetsSouldRun2000ns() throws Exception {
+	@Test
+	public final void getLifeTimeOfTweets() throws Exception {
 		$.importData(lines);
 		$.setupIndex();
-		// TODO: setup timeout
-		$.setupIndex();
+		final long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000; ++i)
+			$.getLifetimeOfTweets("base " + 0);
+		final long end = System.currentTimeMillis();
+		assertTrue("Took " + (end - start) + " millis instead of 20", end
+				- start <= 20);
+	}
+
+	// /**
+	// * Test method for {@link TwitterKnowledgeCenter#importData(String[])}
+	// *
+	// * @throws Exception
+	// */
+	// @Test(timeout = 2 * linesLengthApproximation)
+	// public final void importDataShouldRun2msForEachTweet() throws Exception {
+	// tkcImportData.importData(lines);
+	// }
+	//
+	// /**
+	// * Test method for {@link TwitterKnowledgeCenter#setupIndex()}
+	// *
+	// * @throws Exception
+	// */
+	// @Test(timeout = (2 * linesLengthApproximation / 1000))
+	// public final void setupIndexSouldRun2000nsForEachTweet() throws Exception
+	// {
+	// tkcSetupIndex.setupIndex();
+	// }
+	//
+	// /**
+	// *
+	// * Test method for {@link TwitterKnowledgeCenter#getDailyHistogram()}
+	// *
+	// * @throws Exception
+	// */
+	// @Test(timeout = 20)
+	// public final void getDailyHistogramSouldRun2000ns() throws Exception {
+	// for (int i = 0; i < 10000; ++i)
+	// $.getDailyHistogram();
+	// }
+	//
+	// /**
+	// * Test method for
+	// * {@link TwitterKnowledgeCenter#getLifetimeOfTweets(String)}
+	// *
+	// * @throws Exception
+	// */
+	// @Test(timeout = 20)
+	// public final void getLifeTimeOfTweetsSouldRun2000ns() throws Exception {
+	// int baseTweetNum = 0;
+	// for (int i = 0; i < 10000; ++i) {
+	// $.getLifetimeOfTweets("base " + baseTweetNum);
+	// baseTweetNum = (baseTweetNum + 1) % BASE_TWEETS;
+	// }
+	// }
+
+	/**
+	 * Generate tweets (base tweets & retweets) for a total of
+	 * <code>numBase</code> * (<code>numRetweetsForEach</code> ^
+	 * <code>numLevels</code> - 1) / (<code>numRetweetsForEach</code> - 1)
+	 * tweets.
+	 * 
+	 * @param numBase
+	 *            Number of base tweets.
+	 * @param numRetweetsForEach
+	 *            Number of retweets each tweet has (if it has any retweets).
+	 * @param numLevels
+	 *            How many times a tweet can be recursively retweeted (i.e.
+	 *            retweet of retweet).
+	 * @param seed
+	 *            Seed for randomly generated time stamps of tweets.
+	 * @return <code>numBase</code> base tweets. Each tweet (base or retweet)
+	 *         has <code>numRetweetsForEach</code> retweets, and each retweet
+	 *         has again <code>numRetweetsForEach</code> retweets of its own,
+	 *         and so forth. There are <code>numLevels</code> levels of retweets
+	 *         (i.e. retweet of retweet) for each base tweet.
+	 */
+	private static String[] generateTweets(final int numBase,
+			final int numRetweetsForEach, final int numLevels, final long seed) {
+		final List<String> tweets = new ArrayList<>();
+		final DateFormat dateFormatter = TwitterKnowledgeCenter.injector
+				.getInstance(Key.get(DateFormat.class,
+						Names.named("twitter date formatter")));
+		final Random rnd = new Random(seed);
+		for (int baseTweetNum = 0; baseTweetNum < numBase; ++baseTweetNum) {
+			final String baseId = "base " + baseTweetNum;
+			final long baseDate = rnd.nextLong();
+			tweets.add(dateFormatter.format(new Date(baseDate)) + ", " + baseId);
+			generateRetweets(tweets, baseId, baseDate, dateFormatter, rnd,
+					numRetweetsForEach, numLevels);
+		}
+		Collections.shuffle(tweets, rnd);
+		return tweets.toArray(new String[0]);
+	}
+
+	private static void generateRetweets(final List<String> out,
+			final String originId, final long originDate,
+			final DateFormat dateFormatter, final Random rnd,
+			final int numRetweets, final int numLevels) {
+		if (numLevels <= 0)
+			return;
+		for (int tweetNum = 0; tweetNum < numRetweets; ++tweetNum) {
+			final String id = "re " + tweetNum + " of: " + originId;
+			final long date = originDate + rnd.nextLong();
+			out.add(dateFormatter.format(new Date(date)) + ", " + id + ", "
+					+ originId);
+			generateRetweets(out, id, date, dateFormatter, rnd, numRetweets,
+					numLevels - 1);
+		}
 	}
 
 }
