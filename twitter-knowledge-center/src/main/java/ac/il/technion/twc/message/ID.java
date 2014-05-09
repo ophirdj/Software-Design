564e946e147848b8d2c1d47533cb07ae3f2d511b
Ophir De Jager <ophirdj@gmail.com>
2014-05-09 14:20:42 +0300

    Better hash for ID

--- twitter-knowledge-center/src/main/java/ac/il/technion/twc/message/ID.java
+++ twitter-knowledge-center/src/main/java/ac/il/technion/twc/message/ID.java
@@ -22,10 +22,11 @@ public class ID {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (id == null ? 0 : id.hashCode());
-		return result;
+		// final int prime = 31;
+		// int result = 1;
+		// result = prime * result + (id == null ? 0 : id.hashCode());
+		// return result;
+		return id.hashCode();
 	}
 
 	@Override
