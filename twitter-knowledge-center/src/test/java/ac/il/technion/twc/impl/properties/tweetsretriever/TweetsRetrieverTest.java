72c60582836f633d5bf1496821d33b5613e64071
Ophir De Jager <ophirdj@gmail.com>
2014-05-30 16:09:39 +0300

    Tests for TweetsRetriever

--- twitter-knowledge-center/src/test/java/ac/il/technion/twc/impl/properties/tweetsretriever/TweetsRetrieverTest.java
+++ twitter-knowledge-center/src/test/java/ac/il/technion/twc/impl/properties/tweetsretriever/TweetsRetrieverTest.java
@@ -1,8 +1,15 @@
 package ac.il.technion.twc.impl.properties.tweetsretriever;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
+
+import ac.il.technion.twc.api.tweets.BaseTweet;
+import ac.il.technion.twc.api.tweets.ID;
+import ac.il.technion.twc.api.tweets.Retweet;
 
 /**
  * Test for {@link TweetsRetriever} and {@link TweetsRetrieverBuilder}
@@ -16,8 +23,77 @@ import org.junit.Test;
  */
 public class TweetsRetrieverTest {
 
-  @Test
-  public final void testRetrive() {
-    fail("Not yet implemented"); // TODO
-  }
+	private final TweetsRetrieverBuilder $ = new TweetsRetrieverBuilder();
+
+	@Test
+	public final void getBaseTweetsShouldReturnBaseTweet() {
+		final BaseTweet bt = new BaseTweet(new Date(123456789), new ID(
+				"base tweet ID"));
+		$.visit(bt);
+		assertEquals(bt, $.getResult().getBaseTweets().get(0));
+	}
+
+	@Test
+	public final void getRetweetsShouldReturnRetweet() {
+		final Retweet rt = new Retweet(new Date(123456789),
+				new ID("retweet ID"), new ID("base tweet ID"));
+		$.visit(rt);
+		assertEquals(rt, $.getResult().getRetweets().get(0));
+	}
+
+	@Test
+	public final void getRetweetsShouldReturnNoRetweets() {
+		final BaseTweet bt = new BaseTweet(new Date(123456789), new ID(
+				"base tweet ID"));
+		$.visit(bt);
+		assertEquals(0, $.getResult().getRetweets().size());
+	}
+
+	@Test
+	public final void getBaseTweetsShouldReturnNoBaseTweets() {
+		final Retweet rt = new Retweet(new Date(123456789),
+				new ID("retweet ID"), new ID("base tweet ID"));
+		$.visit(rt);
+		assertEquals(0, $.getResult().getBaseTweets().size());
+	}
+
+	@Test
+	public final void getBaseTweetsShouldReturnBaseTweetsByReceiveOrder() {
+		final BaseTweet bt1 = new BaseTweet(new Date(123456789), new ID(
+				"base tweet 1 ID"));
+		final BaseTweet bt2 = new BaseTweet(new Date(123456789), new ID(
+				"base tweet 2 ID"));
+		$.visit(bt1);
+		$.visit(bt2);
+		final List<BaseTweet> tweets = $.getResult().getBaseTweets();
+		assertEquals(bt1, tweets.get(0));
+		assertEquals(bt2, tweets.get(1));
+	}
+
+	@Test
+	public final void getRetweetsShouldReturnRetweetsByReceiveOrder() {
+		final Retweet rt1 = new Retweet(new Date(123456789), new ID(
+				"retweet 1 ID"), new ID("base tweet ID"));
+		final Retweet rt2 = new Retweet(new Date(123456789), new ID(
+				"retweet 2 ID"), new ID("base tweet ID"));
+		$.visit(rt1);
+		$.visit(rt2);
+		final List<Retweet> tweets = $.getResult().getRetweets();
+		assertEquals(rt1, tweets.get(0));
+		assertEquals(rt2, tweets.get(1));
+	}
+
+	@Test
+	public final void getBaseTweetsShouldReturnBaseTweetAndGetRetweetsShouldReturnRetweet() {
+		final BaseTweet bt = new BaseTweet(new Date(123456789), new ID(
+				"base tweet ID"));
+		final Retweet rt = new Retweet(new Date(123456789),
+				new ID("retweet ID"), new ID("base tweet ID"));
+		$.visit(bt);
+		$.visit(rt);
+		final TweetsRetriever tr = $.getResult();
+		assertEquals(bt, tr.getBaseTweets().get(0));
+		assertEquals(rt, tr.getRetweets().get(0));
+	}
+
 }
