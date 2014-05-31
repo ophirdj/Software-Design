64ac6949e7916d7032b38e2aa3d9cbc8523b7a8d
Ziv Ronen <akarks@gmail.com>
2014-05-31 21:37:54 +0300

    fix acceptance tests

--- twitter-knowledge-center/src/main/java/ac/il/technion/twc/api/core/Storage.java
+++ twitter-knowledge-center/src/main/java/ac/il/technion/twc/api/core/Storage.java
@@ -24,116 +24,114 @@ import com.google.gson.Gson;
  */
 class Storage {
 
-	private final Gson serializer;
-	private final FileHandler fileHandling;
-	private final Map<Class<?>, Future<Object>> retriverByType;
-	private final ExecutorService threadPool;
-	private final Path storePath;
+  private final Gson serializer;
+  private final FileHandler fileHandling;
+  private final Map<Class<?>, Future<Object>> retriverByType;
+  private final ExecutorService threadPool;
+  private final Path storePath;
 
-	/**
-	 * @param gson
-	 * @param fileHandling
-	 * @param threadPool
-	 * 
-	 * @param storageLocation
-	 *            The file directory containing the files. All the instance with
-	 *            the same path have the same group.
-	 */
-	public Storage(final Gson gson, final Path storageLocation,
-			final FileHandler fileHandling, final ExecutorService threadPool) {
-		serializer = gson;
-		this.fileHandling = fileHandling;
-		this.threadPool = threadPool;
-		retriverByType = new HashMap<Class<?>, Future<Object>>();
-		storePath = storageLocation.resolve(Paths.get(getClass()
-				.getSimpleName()));
-	}
+  /**
+   * @param gson
+   * @param fileHandling
+   * @param threadPool
+   * 
+   * @param storageLocation
+   *          The file directory containing the files. All the instance with the
+   *          same path have the same group.
+   */
+  public Storage(final Gson gson, final Path storageLocation,
+      final FileHandler fileHandling, final ExecutorService threadPool) {
+    serializer = gson;
+    this.fileHandling = fileHandling;
+    this.threadPool = threadPool;
+    retriverByType = new HashMap<Class<?>, Future<Object>>();
+    storePath = storageLocation.resolve(Paths.get(getClass().getSimpleName()));
+  }
 
-	/**
-	 * Store a given object. Can only store one object from each type.
-	 * 
-	 * @param type
-	 *            The type of the object
-	 * @param object
-	 *            The object to store.
-	 * @throws IOException
-	 */
-	public void store(final Class<?> type, final Object object)
-			throws IOException {
-		retriverByType.remove(type);
-		fileHandling.store(objectPath(type),
-				serializer.toJson(object, object.getClass()));
-	}
+  /**
+   * Store a given object. Can only store one object from each type.
+   * 
+   * @param type
+   *          The type of the object
+   * @param object
+   *          The object to store.
+   * @throws IOException
+   */
+  public void store(final Class<?> type, final Object object)
+      throws IOException {
+    retriverByType.remove(type);
+    fileHandling.store(objectPath(type),
+        serializer.toJson(object, object.getClass()));
+  }
 
-	/**
-	 * @param type
-	 *            The type to retrive
-	 * @param defualt
-	 *            default return value.
-	 * 
-	 * @return The last stored object of the given type or default if no such
-	 *         value exists.
-	 */
-	public <T> T load(final Class<T> type, final Object defualt) {
-		if (!retriverByType.containsKey(type))
-			prepareSingle(type);
-		final Future<Object> value = retriverByType.get(type);
-		try {
-			// It will be wise to limit the time for such operation
-			return type.cast(value.get());
-		} catch (final InterruptedException e) {
-			throw new RuntimeException(e);
-		} catch (final ExecutionException e) {
-			// TODO: need to handle differently
-			if (e.getCause() instanceof IOException)
-				return type.cast(defualt);
-			else
-				throw new RuntimeException(e.getCause());
-		}
-	}
+  /**
+   * @param type
+   *          The type to retrive
+   * @param defualt
+   *          default return value.
+   * 
+   * @return The last stored object of the given type or default if no such
+   *         value exists.
+   */
+  public <T> T load(final Class<T> type, final Object defualt) {
+    if (!retriverByType.containsKey(type))
+      prepareSingle(type);
+    final Future<Object> value = retriverByType.get(type);
+    try {
+      // It will be wise to limit the time for such operation
+      return type.cast(value.get());
+    } catch (final InterruptedException e) {
+      throw new RuntimeException(e);
+    } catch (final ExecutionException e) {
+      // TODO: need to handle differently
+      if (e.getCause() instanceof IOException)
+        return type.cast(defualt);
+      else
+        throw new RuntimeException(e.getCause());
+    }
+  }
 
-	/**
-	 * Prepare enable the storage to prepare the given types for feature load.
-	 * note: Except from performance, The program should behave the same with or
-	 * without call to that method.
-	 * 
-	 * @param types
-	 *            The types of the objects we want to load
-	 */
-	public void prepare(final Class<?>... types) {
-		for (final Class<?> type : types)
-			try {
-				prepareSingle(type);
-			} catch (final Exception e) {
-				// prepare should not do anything if failed
-			}
-	}
+  /**
+   * Prepare enable the storage to prepare the given types for feature load.
+   * note: Except from performance, The program should behave the same with or
+   * without call to that method.
+   * 
+   * @param types
+   *          The types of the objects we want to load
+   */
+  public void prepare(final Class<?>... types) {
+    for (final Class<?> type : types)
+      try {
+        prepareSingle(type);
+      } catch (final Exception e) {
+        // prepare should not do anything if failed
+      }
+  }
 
-	private void prepareSingle(final Class<?> type) {
-		retriverByType.put(type, threadPool.submit(new Callable<Object>() {
+  private void prepareSingle(final Class<?> type) {
+    retriverByType.put(type, threadPool.submit(new Callable<Object>() {
 
-			@Override
-			public Object call() throws Exception {
-				return serializer.fromJson(fileHandling.load(objectPath(type)),
-						type);
-			}
+      @Override
+      public Object call() throws Exception {
+        return serializer.fromJson(fileHandling.load(objectPath(type)), type);
+      }
 
-		}));
-	}
+    }));
+  }
 
-	/**
-	 * Remove all data that was stored by instance of the same group. The group
-	 * is implementation depended.
-	 * 
-	 * @throws IOException
-	 */
-	public void clear() throws IOException {
-		fileHandling.clear(storePath);
-		retriverByType.clear();
-	}
+  /**
+   * Remove all data that was stored by instance of the same group. The group is
+   * implementation depended.
+   * 
+   * @throws IOException
+   */
+  public void clear() throws IOException {
+    fileHandling.clear(storePath);
+    retriverByType.clear();
+  }
 
-	private <T> Path objectPath(final Class<T> serviceClass) {
-		return storePath.resolve(serviceClass.getCanonicalName());
-	}
+  private <T> Path objectPath(final Class<T> serviceClass) {
+    return storePath.resolve(serviceClass.getCanonicalName());
+  }
 
 }
--- twitter-knowledge-center/src/test/java/acceptance/SimpleUsageTest.java
+++ twitter-knowledge-center/src/test/java/acceptance/SimpleUsageTest.java
@@ -2,10 +2,15 @@ package acceptance;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import ac.il.technion.twc.api.Property;
@@ -29,156 +34,164 @@ import ac.il.technion.twc.api.tweet.Tweet;
  */
 public class SimpleUsageTest {
 
-	@SuppressWarnings("javadoc")
-	public static class MyProperty implements Property {
+  @SuppressWarnings("javadoc")
+  public static class MyProperty implements Property {
 
-		public final int numBase;
-		public final int numRe;
+    public final int numBase;
+    public final int numRe;
 
-		public MyProperty(final List<BaseTweet> baseTweets,
-				final List<Retweet> retweets) {
-			numBase = baseTweets.size();
-			numRe = retweets.size();
-		}
+    public MyProperty(final List<BaseTweet> baseTweets,
+        final List<Retweet> retweets) {
+      numBase = baseTweets.size();
+      numRe = retweets.size();
+    }
 
-		public MyProperty(final List<BaseTweet> baseTweets,
-				final List<Retweet> retweets, final int n) {
-			numBase = baseTweets.size();
-			numRe = retweets.size();
-		}
+    public MyProperty(final List<BaseTweet> baseTweets,
+        final List<Retweet> retweets, final int n) {
+      numBase = baseTweets.size();
+      numRe = retweets.size();
+    }
 
-	}
+  }
 
-	@SuppressWarnings("javadoc")
-	public static class MyQuery implements TwitterQuery {
+  @SuppressWarnings("javadoc")
+  public static class MyQuery implements TwitterQuery {
 
-		public final int numBase;
-		public final int numRe;
+    public final int numBase;
+    public final int numRe;
 
-		@ServiceSetup
-		// Need this to tell TwitterDataCenter to use this constructor when
-		// creating the query
-		public MyQuery(final MyProperty p) {
-			numBase = p.numBase;
-			numRe = p.numRe;
-		}
+    @ServiceSetup
+    // Need this to tell TwitterDataCenter to use this constructor when
+    // creating the query
+    public MyQuery(final MyProperty p) {
+      numBase = p.numBase;
+      numRe = p.numRe;
+    }
 
-		public MyQuery(final MyProperty p, final int m) {
-			numBase = p.numBase;
-			numRe = p.numRe;
-		}
+    public MyQuery(final MyProperty p, final int m) {
+      numBase = p.numBase;
+      numRe = p.numRe;
+    }
 
-	}
+  }
 
-	private List<? extends Tweet> getSomeTweets() {
-		return Arrays.asList(new BaseTweet(new Date(11111), new ID("base 1")),
-				new BaseTweet(new Date(22222), new ID("base 2")), new Retweet(
-						new Date(33333), new ID("retweet"), new ID("tweet")));
-	}
+  private List<? extends Tweet> getSomeTweets() {
+    return Arrays.asList(new BaseTweet(new Date(11111), new ID("base 1")),
+        new BaseTweet(new Date(22222), new ID("base 2")), new Retweet(new Date(
+            33333), new ID("retweet"), new ID("tweet")));
+  }
 
-	private int getNumRetweets(final List<? extends Tweet> tweets) {
-		int numReActual = 0;
-		for (final Tweet t : tweets)
-			if (t instanceof Retweet)
-				++numReActual;
-		return numReActual;
-	}
+  private int getNumRetweets(final List<? extends Tweet> tweets) {
+    int numReActual = 0;
+    for (final Tweet t : tweets)
+      if (t instanceof Retweet)
+        ++numReActual;
+    return numReActual;
+  }
 
-	/**
-	 * Test method for {@link TwitterDataCenterBuilder} and
-	 * {@link TwitterDataCenter}
-	 */
-	@Test
-	public final void simpleUsageTest() {
-		// Create a builder for the data center
-		final TwitterDataCenterBuilder builder = new TwitterSystemBuilder();
-		// Add wanted properties and queries
-		builder.addProperty(MyProperty.class).registerQuery(MyQuery.class);
-		// Create the data center
-		final TwitterDataCenter dataCenter = builder.build();
+  @Before
+  public void setup() throws IOException {
+    FileUtils.cleanDirectory(Paths.get("system", "Storage").toFile());
+  }
 
-		// Let's get some tweets and count how many are retweets
-		final List<? extends Tweet> tweets = getSomeTweets();
-		final int numReActual = getNumRetweets(tweets);
+  @After
+  public void tearDown() throws IOException {
+    FileUtils.cleanDirectory(Paths.get("system", "Storage").toFile());
+  }
 
-		// Import the tweets
-		dataCenter.importData(tweets);
-		// Evaluate the queries
-		dataCenter.loadServices();
-		// Now we can ask the queries
-		final MyQuery q = dataCenter.getService(MyQuery.class);
-		assertEquals(numReActual, q.numRe);
-		assertEquals(tweets.size() - numReActual, q.numBase);
+  /**
+   * Test method for {@link TwitterDataCenterBuilder} and
+   * {@link TwitterDataCenter}
+   */
+  @Test
+  public final void simpleUsageTest() {
+    // Create a builder for the data center
+    final TwitterDataCenterBuilder builder = new TwitterSystemBuilder();
+    // Add wanted properties and queries
+    builder.addProperty(MyProperty.class).registerQuery(MyQuery.class);
+    // Create the data center
+    final TwitterDataCenter dataCenter = builder.build();
 
-		// cleanup of persistent storage
-		dataCenter.clear();
-	}
+    // Let's get some tweets and count how many are retweets
+    final List<? extends Tweet> tweets = getSomeTweets();
+    final int numReActual = getNumRetweets(tweets);
 
-	/**
-	 * Test method for {@link TwitterDataCenterBuilder} and
-	 * {@link TwitterDataCenter}
-	 */
-	@Test
-	public final void simpleUsageTestWithoutTweets() {
-		// Add wanted properties and queries and create the data center
-		final TwitterDataCenter dataCenter = new TwitterSystemBuilder()
-				.addProperty(MyProperty.class).registerQuery(MyQuery.class)
-				.build();
-		// Evaluate the queries
-		dataCenter.loadServices();
-		// Now we can ask the queries
-		final MyQuery q = dataCenter.getService(MyQuery.class);
-		assertEquals(0, q.numRe);
-		assertEquals(0, q.numBase);
-	}
+    // Import the tweets
+    dataCenter.importData(tweets);
+    // Evaluate the queries
+    dataCenter.loadServices();
+    // Now we can ask the queries
+    final MyQuery q = dataCenter.getService(MyQuery.class);
+    assertEquals(numReActual, q.numRe);
+    assertEquals(tweets.size() - numReActual, q.numBase);
 
-	/**
-	 * Test method for {@link TwitterDataCenterBuilder} and
-	 * {@link TwitterDataCenter}
-	 */
-	@Test
-	public final void simpleUsageTestWithFactories() {
-		// Create a builder for the data center
-		final TwitterDataCenterBuilder builder = new TwitterSystemBuilder();
-		// Add wanted properties using factories
-		builder.addProperty(MyProperty.class,
-				new PropertyFactory<MyProperty>() {
+    // cleanup of persistent storage
+    dataCenter.clear();
+  }
 
-					@Override
-					public MyProperty get(final List<BaseTweet> baseTweets,
-							final List<Retweet> retweets) {
-						return new MyProperty(baseTweets, retweets, 0);
-					}
-				});
-		// Add wanted queries using factories
-		builder.registerQuery(MyQuery.class,
-				new TwitterQueryFactory<MyQuery>() {
+  /**
+   * Test method for {@link TwitterDataCenterBuilder} and
+   * {@link TwitterDataCenter}
+   */
+  @Test
+  public final void simpleUsageTestWithoutTweets() {
+    // Add wanted properties and queries and create the data center
+    final TwitterDataCenter dataCenter =
+        new TwitterSystemBuilder().addProperty(MyProperty.class)
+            .registerQuery(MyQuery.class).build();
+    // Evaluate the queries
+    dataCenter.loadServices();
+    // Now we can ask the queries
+    final MyQuery q = dataCenter.getService(MyQuery.class);
+    assertEquals(0, q.numRe);
+    assertEquals(0, q.numBase);
+  }
 
-					@SuppressWarnings("unused")
-					public MyQuery get(final MyProperty p) {
-						return new MyQuery(p, 0);
-					}
+  /**
+   * Test method for {@link TwitterDataCenterBuilder} and
+   * {@link TwitterDataCenter}
+   */
+  @Test
+  public final void simpleUsageTestWithFactories() {
+    // Create a builder for the data center
+    final TwitterDataCenterBuilder builder = new TwitterSystemBuilder();
+    // Add wanted properties using factories
+    builder.addProperty(MyProperty.class, new PropertyFactory<MyProperty>() {
 
-				});
+      @Override
+      public MyProperty get(final List<BaseTweet> baseTweets,
+          final List<Retweet> retweets) {
+        return new MyProperty(baseTweets, retweets, 0);
+      }
+    });
+    // Add wanted queries using factories
+    builder.registerQuery(MyQuery.class, new TwitterQueryFactory<MyQuery>() {
 
-		// Create the data center
-		final TwitterDataCenter dataCenter = builder.build();
+      @SuppressWarnings("unused")
+      public MyQuery get(final MyProperty p) {
+        return new MyQuery(p, 0);
+      }
 
-		// Let's get some tweets and count how many are retweets
-		final List<? extends Tweet> tweets = getSomeTweets();
-		final int numReActual = getNumRetweets(tweets);
+    });
 
-		// Import the tweets
-		dataCenter.importData(tweets);
-		// Evaluate the queries
-		dataCenter.loadServices();
-		// Now we can ask the queries
-		final MyQuery q = dataCenter.getService(MyQuery.class);
-		assertEquals(numReActual, q.numRe);
-		assertEquals(tweets.size() - numReActual, q.numBase);
+    // Create the data center
+    final TwitterDataCenter dataCenter = builder.build();
 
-		// cleanup of persistent storage
-		dataCenter.clear();
-	}
+    // Let's get some tweets and count how many are retweets
+    final List<? extends Tweet> tweets = getSomeTweets();
+    final int numReActual = getNumRetweets(tweets);
+
+    // Import the tweets
+    dataCenter.importData(tweets);
+    // Evaluate the queries
+    dataCenter.loadServices();
+    // Now we can ask the queries
+    final MyQuery q = dataCenter.getService(MyQuery.class);
+    assertEquals(numReActual, q.numRe);
+    assertEquals(tweets.size() - numReActual, q.numBase);
+
+    // cleanup of persistent storage
+    dataCenter.clear();
+  }
 
 }
