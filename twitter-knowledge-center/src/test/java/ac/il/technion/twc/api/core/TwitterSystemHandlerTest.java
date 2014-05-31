406106b7e522566ff1222e7b4571e9a00f9ad07d
Ziv Ronen <akarks@gmail.com>
2014-05-31 19:41:27 +0300

    Fix a test

--- twitter-knowledge-center/src/test/java/ac/il/technion/twc/api/core/TwitterSystemHandlerTest.java
+++ twitter-knowledge-center/src/test/java/ac/il/technion/twc/api/core/TwitterSystemHandlerTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -16,6 +17,11 @@ import java.util.Set;
 import org.junit.Test;
 
 import ac.il.technion.twc.api.TwitterQuery;
+import ac.il.technion.twc.api.core.TwitterSystemHandler.Tweets;
+import ac.il.technion.twc.api.tweet.BaseTweet;
+import ac.il.technion.twc.api.tweet.ID;
+import ac.il.technion.twc.api.tweet.Retweet;
+import ac.il.technion.twc.api.tweet.Tweet;
 
 /**
  * Test class for {@link TwitterSystemHandler} and {@link TwitterSystemBuilder}
@@ -57,38 +63,36 @@ public class TwitterSystemHandlerTest {
   public final void importDataShouldCallStorageLoadWithEachService()
       throws InstantiationException, IllegalAccessException,
       IllegalArgumentException, InvocationTargetException, IOException {
-    // final Tweets mockOldTweets = mock(Tweets.class);
-    // final BaseTweet baseTweet = new BaseTweet(new Date(1L), new ID("2763"));
-    // final Retweet retweet =
-    // new Retweet(new Date(2L), new ID("4256"), new ID("2763"));
-    // final List<Tweet> mockNewTweets = Arrays.asList(baseTweet, retweet);
-    // when(storageMock.load(new Tweets())).thenReturn(mockOldTweets);
-    // final BaseTweet baseTweet2 =
-    // new BaseTweet(new Date(3756L), new ID("356876"));
-    // final Retweet retweet2 =
-    // new Retweet(new Date(3865L), new ID("46464774"), new ID("2354362"));
-    // when(mockOldTweets.getBaseTweets()).thenReturn(Arrays.asList(baseTweet2));
-    // when(mockOldTweets.getRetweets()).thenReturn(Arrays.asList(retweet2));
-    //
-    // final List<Object> os = new ArrayList<>();
-    // final Integer defaultInteger = new Integer(0);
-    // final Long defaultLong = new Long(0);
-    // os.add(defaultInteger);
-    // os.add(defaultLong);
-    //
-    // when(services.iterator()).thenReturn(os.iterator());
-    // final Integer integerValue = new Integer(5);
-    // when(serviceBuilder.getInstance(Integer.class)).thenReturn(integerValue);
-    // final Long longValue = new Long(5L);
-    // when(serviceBuilder.getInstance(Long.class)).thenReturn(longValue);
-    // $.importData(mockNewTweets);
-    // verify(storageMock).store(
-    // new Tweets(Arrays.asList(baseTweet, baseTweet2, retweet, retweet2)));
-    // verify(serviceBuilder).setProperties(Arrays.asList(baseTweet,
-    // baseTweet2),
-    // Arrays.asList(retweet, retweet2));
-    // verify(storageMock).store(integerValue);
-    // verify(storageMock).store(longValue);
+    final Tweets mockOldTweets = mock(Tweets.class);
+    final BaseTweet baseTweet = new BaseTweet(new Date(1L), new ID("2763"));
+    final Retweet retweet =
+        new Retweet(new Date(2L), new ID("4256"), new ID("2763"));
+    final List<Tweet> mockNewTweets = Arrays.asList(baseTweet, retweet);
+    when(storageMock.load(Tweets.class, new Tweets()))
+        .thenReturn(mockOldTweets);
+    final BaseTweet baseTweet2 =
+        new BaseTweet(new Date(3756L), new ID("356876"));
+    final Retweet retweet2 =
+        new Retweet(new Date(3865L), new ID("46464774"), new ID("2354362"));
+    when(mockOldTweets.getBaseTweets()).thenReturn(Arrays.asList(baseTweet2));
+    when(mockOldTweets.getRetweets()).thenReturn(Arrays.asList(retweet2));
+
+    final Class<ServiceOne> c1 = ServiceOne.class;
+    final Class<ServiceTwo> c2 = ServiceTwo.class;
+    final List<Class<? extends TwitterQuery>> os = Arrays.asList(c1, c2);
+    when(services.iterator()).thenReturn(os.iterator());
+
+    final ServiceOne o1 = new ServiceOne();
+    when(serviceBuilder.getInstance(c1)).thenReturn(o1);
+    final ServiceOne o2 = new ServiceOne();
+    when(serviceBuilder.getInstance(c2)).thenReturn(o2);
+    $.importData(mockNewTweets);
+    verify(storageMock).store(Tweets.class,
+        new Tweets(Arrays.asList(baseTweet, baseTweet2, retweet, retweet2)));
+    verify(serviceBuilder).setProperties(Arrays.asList(baseTweet, baseTweet2),
+        Arrays.asList(retweet, retweet2));
+    verify(storageMock).store(c1, o1);
+    verify(storageMock).store(c2, o2);
   }
 
   /**
